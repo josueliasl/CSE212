335713c285a2ab53ea9927fc3beb21f685222516
josueliasl <josue.elias.leyva@gmail.com>
2025-06-30 22:50:42 -0600

    DONE!

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -9,11 +9,19 @@ public static class Arrays
     public static double[] MultiplesOf(double number, int length)
     {
         // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // PLAN:
+        // 1.- Create a double array named multiples with the size of length
+        // 2.- Begin a for loop starting at point 0 until length using variable called i
+        // 3.- Multiply number for i+ 1 in order to not start with cero
+        // 4.- Store the result of the previous calculous in multiples with array i
+        // 5.- Once the loops is over return multiples
 
-        return []; // replace this return statement with your own
+        double[] multiples = new double[length];
+        for (var i = 0; i < length; ++i)
+        {
+                multiples[i] = number * (i + 1);
+        }
+        return multiples;
     }
 
     /// <summary>
@@ -26,8 +34,18 @@ public static class Arrays
     public static void RotateListRight(List<int> data, int amount)
     {
         // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // PLAN
+        // 1.- Create a variable called  startIndex to substract amount from the data count 
+        // 2.- Create a list of int called endPart to get the range of data having startIndex and amount as parameters
+        // 3.- Create a list of int called startPart to get the range from 0 to startIndex as parameters
+        // 4.- Clear data list
+        // 5.- Use AddRange to add endpart and startPart to data in their corresponding order
+        var startIndex = data.Count - amount;
+        List<int> endPart = data.GetRange(startIndex, amount);
+        List<int> startPart = data.GetRange(0, startIndex);
+        data.Clear();
+        data.AddRange(endPart);
+        data.AddRange(startPart);
+
     }
 }
--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -3,14 +3,39 @@ public static class ArraySelector
     public static void Run()
     {
         var l1 = new[] { 1, 2, 3, 4, 5 };
-        var l2 = new[] { 2, 4, 6, 8, 10};
-        var select = new[] { 1, 1, 1, 2, 2, 1, 2, 2, 2, 1};
+        var l2 = new[] { 2, 4, 6, 8, 10 };
+        var select = new[] { 1, 1, 1, 2, 2, 1, 2, 2, 2, 1 };
         var intResult = ListSelector(l1, l2, select);
         Console.WriteLine("<int[]>{" + string.Join(", ", intResult) + "}"); // <int[]>{1, 2, 3, 2, 4, 4, 6, 8, 10, 5}
     }
 
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
     {
-        return [];
+        var result = new int[select.Length];
+        int index1 = 0;
+        int index2 = 0;
+
+        for (var i = 0; i < select.Length; i++)
+        {
+            if (select[i] == 1)
+                result[i] = list1[index1++];
+            else
+                result[i] = list2[index2++];
+        }
+
+        return result;
+    }
+
+    private static char[] ListSelector(char[] list1, char[] list2, int[] select)
+    {
+        var result = new char[select.Length];
+        var index1 = 0;
+        int index2 = 0;
+
+        for (var i = 0; i < select.Length; i++)
+        {
+            result[i] = select[i] == 1 ? list1[index1++] : list2[index2++];
+        }
+        return result;
     }
 }
